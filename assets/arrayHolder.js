60eaaec899e1b3b8adff4d91ffdab36c092c4c5e
mhoppenrath <mmhoppenrath@gmail.com>
2017-07-29 15:50:05 -0500

    Can't refreference the array

--- assets/arrayHolder.js
+++ assets/arrayHolder.js
@@ -1,11 +1,11 @@
 // this is the arrays for the absurd amount of questions. 
 //The arrays will hold objects that will have questions answers and the correct answer value
 
-var GoTfactoids = [ theWallBuilder
+var GoTfactoids = [ theWallBuilder, queenDanyTitles, AzorAzahi, jofferysKiller
 
 	]
 var theWallBuilder = {
-	"question": "Who was the builder of the wall",
+	"question": "Who was the builder of the wall?",
 	answers: [
 		"Bran the Builder",
 		"The Night King",
@@ -13,6 +13,46 @@ var theWallBuilder = {
 		"The Children of the Forest"
 	],
 	"correctAnswer": "Bran the Builder",
-	"src": "assets/images/Bran.jpeg",
+	"src": "images/Bran.jpeg",
 	"previousDone": false
 }
+var queenDanyTitles = {
+	"question": "Which of these is not one of Daenerys Targaryen's current titles?",
+	answers: ["Storm Born",
+		 	"Mother of Dragons",
+		 	"Breaker of Chains",
+		 	"The Fire Reborn"
+		 	],
+	"correctAnswer": "The Fire Reborn",
+	"src": "images/Danny.png",
+	"previousDone": false
+
+}
+
+var AzorAzahi = {
+	"question": "Who is not potentially Azor Azahi reborn?",
+	answers: [
+			"John Snow",
+			"Daenerys  Targaryen",
+			"Stannis Baratheon",
+			"Rhaegar Targaryen"
+	],
+	"correctAnswer":"Rhaegar Targaryen",
+	"src": "images/Azor_Ahai",
+	"previousDone": false
+}
+
+var jofferysKiller = {
+	"question": "Who killed Joffery Baratheon (Lannister)?",
+	answers: [
+		"Sansa Stark",
+		"Tyrion Lannister",
+		"Olena Tyrell",
+		"Peter Baylish"
+	],
+	"correctAnswer":"Olena Tyrell",
+	"src": "images/Olenna.jpg",
+	"previousDone":false
+
+}
+
--- assets/game.js
+++ assets/game.js
@@ -1,43 +1,117 @@
 $(document).ready(function() {
 	// set variables for inital start
 	var score = 0; //keeps track of the score which will print out at the end
-	var firstQuestion = true; //var for if it is a new game or not
 	var questionCount = GoTfactoids.length; //makes the end of the game
 	var progress = 0; //this tracks the progress ingame so far
+	var unanswered = 0;
 	var intervalId; // the counter variable
 	var randomPicker; //which question is picked. This is global because I want it to go between functions
+	var currentQuestion; //this is global so we can share it amongst all the functions
+	var nextQuestion = "<p class='text-center main-button-container'><a class='btn btn-primary btn-lg btn-block next-button' href='#' role='button'>Next Question</a></p>"
 
-	//set the timer that will stop when  it goes to an answer screen and restart when it goes to question
-	$('#hiddenResult').on('click', function() {
-		if (firstQuestion) {
-			firstQuestion = false;
-        	intervalId = setInterval(firstTimerStart, 1000);
-        	timeLeft = 30;
-        	newQuestion()
-        }
-	});
-	function firstTimerStart () { // creates the time and when it hits zero it goes to the annswer screen
-    	timeLeft --;
-    	$('#timer').html(timeLeft);
-    	if (timer === 0) {
-    		img = ()
-    	}
-
-
+	//creates a start button under the title
+	function initalScreen() {
+		startScreen = "<p class='text-center main-button-container'><a class='btn btn-primary btn-lg btn-block start-button' href='#' role='button'>Start Quiz</a></p>";
+		$("#questionHolder").html(startScreen);
 	}
-	function answeredQuestion() {
-		clearInterval(intervalId);
+	initalScreen();
 
-	}
-	function newQuestion() {
-	randomPicker = Math.floor(Math.random() * (questionCount));
-	if (!GoTfactoids[randomPicker].previousDone) {
-		$('#questionHolder').html(GoTfactoids[randomPicker].question)
-			for (i = 0; i<4;i++){
-			$('#answersHolder').append(GoTfactoids[randomPicker].answer[i] + "<br>")
+
+	// creates the timer function
+	function timer() {
+		theTime = setInterval(thirtySeconds, 1000);
+		function thirtySeconds(){
+			if (counter === 0) {
+				clearInterval(theTime);
+				timeoutLoss();
 			}
+			if (counter > 0 ) {
+				counter--;
+			}
+			$("#timer").html("0:" + counter);
 		}
-	else { newQuestion()}
+
 	}
+	//times out the timer when you reach 0
+	function timeOutLoss() {
+		unanswered++;
+		progress ++;
+		console.log("unanswered: " + unanswered + " progress: " + progress);
+		$("#timer").html("0:00");
+		$("questionHolder").html("You ran out of time. The correct answer was: " + GoTfactoids[currentQuestion].correctAnswer);
+		//put image in the question hodler
+		var anwserImage = 
+		$("#hiddenResult").html(nextQuestion);
+	}
+	function pickedRightAnswer() {
+		score++;
+		progress ++;
+		console.log("score: " + score + " progress: " + progress);		
+		$("#questionHolder").html("Correct!");
+		//put image in the question hodler
+		$("#hiddenResult").html(nextQuestion);
+
+	}
+	//clearly for wrong answers
+	function pickedWrongAnswer() {
+		progess++;
+		console.log("Progress: " + progress);
+		$("#questionHolder").html("WRONG! The right answer is" + GoTfactoids[currentQuestion].correctAnswer);
+		$("#hiddenResult").html(nextQuestion);
+		var answerImage = $('<img>');
+		answerImage.attr('src', GoTfactoids[i].src);
+	}
+	// first we pick a radom number then we tet to see if we have done it.
+	function randomQuestion() {
+		randomPicker = Math.floor(Math.random() * questionCount);
+		console.log(randomPicker);
+		return randomPicker;
+	}
+	function buildQuestion() {
+		if (progress != questionCount) {
+			do {
+				currentQuestion = randomQuestion();
+				console.log(GoTfactoids[currentQuestion])
+			} while(GoTfactoids[currentQuestion].previousDone === true);
+			GoTfactoids[currentQuestion].previousDone = true;
+			for (var i = 0; i<4;i++){
+				potientialAnswer = GoTfactoids[currentQuestion].answers[i];
+				questioniHTML = "<p class='main-button-container'><a class='btn btn-primary btn-lg btn-block answer-button' value: " + GoTfactoids[i] +  "href='#' role='button'>" + potientialAnswer + "</a></p>"
+
+			}
+		}	
+		else {
+			newGame();
+		}
+
+	}
+
+	function newGame() {
+		counter = 30;
+		progress = 0;
+		score=0;
+		unanswered=0;
+		for (var i = 0; i<GoTfactoids.length;i++){
+			GoTfactoids[i].previousDone = false;
+		}
+		newStartScreen = "<p class='text-center main-button-container'><a class='btn btn-primary btn-lg btn-block start-button' href='#' role='button'>Start a New Game</a></p>";
+		$("#questionHolder").html(newStartScreen);
+
+	}
+	$('.start-button, .next-button').on("click", function(){
+		buildQuestion()
+		timer();
+	});
+
+	$('.answer-buttonns').on('click', function(){
+		if(this.value = GoTfactoids[currentQuestion]) {
+			pickedRightAnswer();
+		}
+		else {
+			pickedWrongAnswer();
+		}
+	})
+
+
 
 });
